4e00a7a3a0692f7be62e92941252e2b07f09fee9
Laura Jodz <laurajodz@gmail.com>
2018-04-05 22:04:09 -0700

    edit inputs

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -2,23 +2,40 @@ import React, {Component} from 'react';
 import {reduxForm, Field} from 'redux-form';
 
 export class ContactForm extends Component {
-    
+
     onSubmit(values) {
-        console.log(values);
+      console.log(values);
     }
-    
+
     render() {
         return (
-            <form onSubmit={this.props.handleSubmit(values =>
-                    this.onSubmit(values))}
+            <form
+              onSubmit={this.props.handleSubmit(values =>
+                  this.onSubmit(values))}
             >
-                <label htmlFor="name">Your Name</label>
-                <Field type="text" id="name" name="name" component="input" validate={[required, nonEmpty]} />
+              <label htmlFor="name">Your Name</label>
+              <Field
+                  type="text"
+                  id="name"
+                  name="name"
+                  component="input"
+                />
                 <label htmlFor="email">Email address</label>
-                <Field type="email" id="email" name="email" component="input" validate={[required, nonEmpty, email]} />
+                <Field
+                  type="email"
+                  id="email"
+                  name="email"
+                  component="input"
+                />
                 <label htmlFor="message">Message</label>
-                <Field id="message" name="message" component="textarea" validate={[required, nonEmpty]} />
-                <button type="submit">Submit</button>
+                <Field
+                  id="message"
+                  name="message"
+                  component="textarea"
+                />
+                <button
+                  type="submit">Submit
+                </button>
             </form>
         );
     }
